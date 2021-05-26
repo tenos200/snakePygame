998a917fd704fb793f06401ca1b2cd78bc63f96c
Tim Enos CS2019 <tim.enos.2020@uni.strath.ac.uk>
2021-05-26 22:31:25 +0200

    removed some stuff

--- snake.py
+++ snake.py
@@ -21,12 +21,6 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 game = False;
-        keys = pg.key.get_pressed()
-        if keys[pg.K_RIGHT] and x < window_width - x:
-            x+=velocity
-        if keys[pg.K_LEFT] and x > 0:
-            x+-velocity
-
         pg.draw.rect(board, (0, 0, 0), (x, y, 20, 20))
         move(x, y)
         pg.display.update()
