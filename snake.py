ad955ab123e3d4627235f08119fa493b1f106da7
Tim Enos CS2019 <tim.enos.2020@uni.strath.ac.uk>
2021-06-01 22:52:56 +0200

    restructured to OOP

--- snake.py
+++ snake.py
@@ -1,95 +1,54 @@
 import pygame as pg
+from pygame.math import Vector2 as v2
 import time
 import sys
 import random
 
 
+class Snake:
+    def __init__(self):
+        self.body = [v2(5, 10), v2(6, 10), v2(7, 10)]
+    def draw_snake(self):
+        for block in self.body:
+            snake_rect = pg.Rect(block.x * cell_size, block.y * cell_size, cell_size, cell_size)
+            pg.draw.rect(board, (0, 0, 0), snake_rect)
+        
+
+class Food:
+    def __init__(self):
+        self.x = random.randint(0, cell_number - 1) 
+        self.y = random.randint(0, cell_number - 1) 
+        self.vector = v2(self.x, self.y)
+
+    def place_food(self):
+        food_rect = pg.Rect(self.vector.x * cell_size, self.vector.y * cell_size, cell_size, cell_size)
+        pg.draw.rect(board, (255, 0, 0), food_rect)
+
+
+
+
+pg.init()
+clock = pg.time.Clock()
+framerate = 60
 bg_color = (175, 215, 75)
-game_over_color = (0, 0, 0) 
-window_height = 30
-window_width = 20
-height = 20
-width = 20
-x = 50
-y = 50
-velocity = 2 
+cell_size = 40 
+cell_number = 20
+window_width = 800 
+game = True
+board = pg.display.set_mode((cell_size * cell_number, cell_size * cell_number))
+pg.display.set_caption('Snake by Tenos200')
 
+fruit = Food()
+snake = Snake()
 
-def main():
-    x = 50
-    y = 50
-    x_change = 0
-    y_change = 0
-    height = 20
-    width = 20
-    rand_x = random.randint(1, window_width*window_height - width)
-    rand_y = random.randint(1, window_height*window_width - height)
-    pg.init()
-    board = pg.display.set_mode((window_width * window_height, window_height * window_width))
-    clock = pg.time.Clock()
-    clock.tick(60)
-    pg.display.set_caption('Snake')
+while game:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            game = False
 
-    game = True
-    game_over = False
-
-    while game:
-        while game_over:
-            font = pg.font.Font('freesansbold.ttf', 22)
-            msg = font.render("Game over! Press P to play again or Q to quit", True, (255, 0, 0))
-            board.fill(game_over_color)
-            board.blit(msg, (80, 250))
-            pg.display.update()
-            for event in pg.event.get():
-                if event.type == pg.KEYDOWN:
-                    if event.key == pg.K_q:
-                        quit()
-                    elif event.key == pg.K_p:
-                        main()
-
-
-
-
-            
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                pg.quit()
-                sys.exit()
-            if event.type == pg.KEYDOWN:
-                keys = pg.key.get_pressed()
-                if event.key == pg.K_LEFT:
-                    x_change = -velocity
-                    y_change = 0
-                elif event.key == pg.K_RIGHT:
-                    x_change = velocity
-                    y_change = 0
-                elif event.key == pg.K_UP:
-                    y_change = -velocity
-                    x_change = 0
-                elif event.key == pg.K_DOWN:
-                    y_change = velocity
-                    x_change = 0  
-
-        y+=y_change
-        x+=x_change
-        board.fill(bg_color)
-
-        pg.draw.rect(board, (0, 0, 0), (x, y, height, width))
-        pg.draw.rect(board, (255, 0, 0), (rand_x, rand_y, height, width))
-        clock.tick(60)
-        pg.display.update()
-
-        if x >= 580 or x < 0 or y >= 580 or y < 0:
-            x_change = 0
-            y_change = 0
-            game_over = True
-        if x == rand_x and y == rand_y:
-            print("Yummy!")
-
-def quit():
-    print("quitting")
-    pg.quit()
-    sys.exit()
-
-if __name__ == "__main__":
-    main()
+    board.fill(bg_color)
+    snake.draw_snake()
+    fruit.place_food()
+    pg.display.update()
+    clock.tick(framerate)
+    
