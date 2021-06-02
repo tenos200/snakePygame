ae102b85b2d1d076d93744d38f4f66470ead51db
Tim Enos CS2019 <tim.enos.2020@uni.strath.ac.uk>
2021-06-02 22:22:44 +0200

    food spawns in new spot when collision occurs

--- snake.py
+++ snake.py
@@ -7,12 +7,20 @@ import random
 
 class Snake:
     def __init__(self):
-        self.body = [v2(5, 10), v2(6, 10), v2(7, 10)]
+        self.body = [v2(5, 10), v2(6, 10)]
+        self.movement = v2(1, 0)
+
     def draw_snake(self):
         for block in self.body:
             snake_rect = pg.Rect(block.x * cell_size, block.y * cell_size, cell_size, cell_size)
             pg.draw.rect(board, (0, 0, 0), snake_rect)
-        
+
+    def move_snake(self):
+        copy_body = self.body[:-1]
+        copy_body.insert(0, copy_body[0] + self.movement)
+        self.body = copy_body
+
+
 
 class Food:
     def __init__(self):
@@ -23,6 +31,25 @@ class Food:
     def place_food(self):
         food_rect = pg.Rect(self.vector.x * cell_size, self.vector.y * cell_size, cell_size, cell_size)
         pg.draw.rect(board, (255, 0, 0), food_rect)
+
+
+
+
+class Main:
+    def __init__(self):
+        self.snake = Snake()
+        self.food = Food()
+    def update(self):
+        self.snake.move_snake()
+    def draw_elements(self):
+        self.snake.draw_snake()
+        self.food.place_food()
+    def check_position(self):
+        if self.food.vector == self.snake.body[0]:
+            print("Test")
+            self.food = Food()
+            #reposition the fruit
+            # add another block to the snake
 
 
 
@@ -38,17 +65,33 @@ game = True
 board = pg.display.set_mode((cell_size * cell_number, cell_size * cell_number))
 pg.display.set_caption('Snake by Tenos200')
 
-fruit = Food()
-snake = Snake()
+
+
+SCREEN_UPDATE = pg.USEREVENT
+pg.time.set_timer(SCREEN_UPDATE, 150)
+
+game = Main()
 
 while game:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             game = False
+        if event.type == SCREEN_UPDATE:
+            game.update()
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_RIGHT:
+                game.snake.movement = (1, 0)
+            if event.key == pg.K_LEFT:
+                game.snake.movement = (-1, 0)
+            if event.key == pg.K_UP:
+                game.snake.movement = (0, -1)
+            if event.key == pg.K_DOWN:
+                game.snake.movement = (0, 1)
+
 
     board.fill(bg_color)
-    snake.draw_snake()
-    fruit.place_food()
+    game.draw_elements()
+    game.check_position()
     pg.display.update()
     clock.tick(framerate)
     
